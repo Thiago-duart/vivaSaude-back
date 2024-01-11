dd9af35efc144d7266aa52c95beb06691908d969
ThiagoDuart <td1405150+github@gmail.com>
2024-01-11 15:15:27 -0300

    refactor: simplify code

--- src/presentation/controller/singUp/SingUp.ts
+++ src/presentation/controller/singUp/SingUp.ts
@@ -1,14 +1,13 @@
+import { badRequest } from "../../helper";
 import { IHttpRequest, IHttpResponse, ISingUp } from "../../protocols";
+import { MissingParamError } from "../../errors";
 
 export class SingUpController implements ISingUp {
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
     const data = ["email", "name", "password"];
     for (let value of data) {
       if (!request.body[value]) {
-        return {
-          statusCode: 400,
-          body: new Error(`Missing param: ${value}`),
-        };
+        return badRequest(new MissingParamError(value));
       }
     }
   }
