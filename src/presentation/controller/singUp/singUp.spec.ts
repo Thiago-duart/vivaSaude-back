e54d1fd281dc4d5f499145cc6f67bdb6c0f05ce7
ThiagoDuart <td1405150+github@gmail.com>
2024-01-11 14:49:35 -0300

    test: return 400 if email and password is not provided

--- src/presentation/controller/singUp/singUp.spec.ts
+++ src/presentation/controller/singUp/singUp.spec.ts
@@ -15,6 +15,30 @@ describe("singUp controller", () => {
     };
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("missing param: name"));
+    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+  });
+  test("should return 400 if email is not provided", async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        password: "valid_password",
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: email"));
+  });
+  test("should return 400 if password is not provided", async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        email: "valid_email",
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: password"));
   });
 });
