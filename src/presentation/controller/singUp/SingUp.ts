b50327643b0ae9b2c8490947bc30195a2c6e5cad
ThiagoDuart <td1405150+github@gmail.com>
2024-01-11 15:49:41 -0300

    feat: return 400 if email is not valid

--- src/presentation/controller/singUp/SingUp.ts
+++ src/presentation/controller/singUp/SingUp.ts
@@ -1,14 +1,28 @@
 import { badRequest } from "../../helper";
-import { IHttpRequest, IHttpResponse, ISingUp } from "../../protocols";
-import { MissingParamError } from "../../errors";
+import {
+  IEmailValidator,
+  IHttpRequest,
+  IHttpResponse,
+  ISingUp,
+} from "../../protocols";
+import { InvalidParamError, MissingParamError } from "../../errors";
 
 export class SingUpController implements ISingUp {
+  private readonly emailValidator: IEmailValidator;
+  constructor(emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator;
+  }
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
     const data = ["email", "name", "password"];
     for (let value of data) {
       if (!request.body[value]) {
         return badRequest(new MissingParamError(value));
       }
+    }
+    const { email } = request.body;
+    const isValid = this.emailValidator.isValid(email);
+    if (!isValid) {
+      return badRequest(new InvalidParamError("email"));
     }
   }
 }
