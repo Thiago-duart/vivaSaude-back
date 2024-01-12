08a91f7130fa84794f6994afd553f9e62a9d2556
ThiagoDuart <td1405150+github@gmail.com>
2024-01-12 14:59:43 -0300

    feat: should return 500 if (isValid) return exception

--- src/presentation/controller/singUp/SingUp.ts
+++ src/presentation/controller/singUp/SingUp.ts
@@ -1,11 +1,15 @@
-import { badRequest } from "../../helper";
+import { badRequest, serverError } from "../../helper";
 import {
   IEmailValidator,
   IHttpRequest,
   IHttpResponse,
   ISingUp,
 } from "../../protocols";
-import { InvalidParamError, MissingParamError } from "../../errors";
+import {
+  InvalidParamError,
+  MissingParamError,
+  ServerError,
+} from "../../errors";
 
 export class SingUpController implements ISingUp {
   private readonly emailValidator: IEmailValidator;
@@ -13,16 +17,20 @@ export class SingUpController implements ISingUp {
     this.emailValidator = emailValidator;
   }
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
-    const data = ["email", "name", "password"];
-    for (let value of data) {
-      if (!request.body[value]) {
-        return badRequest(new MissingParamError(value));
+    try {
+      const data = ["email", "name", "password"];
+      for (let value of data) {
+        if (!request.body[value]) {
+          return badRequest(new MissingParamError(value));
+        }
       }
-    }
-    const { email } = request.body;
-    const isValid = this.emailValidator.isValid(email);
-    if (!isValid) {
-      return badRequest(new InvalidParamError("email"));
+      const { email } = request.body;
+      const isValid = this.emailValidator.isValid(email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
+    } catch (error) {
+      return serverError(new ServerError());
     }
   }
 }
