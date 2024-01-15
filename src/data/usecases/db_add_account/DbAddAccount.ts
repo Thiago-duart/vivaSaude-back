78b6d5a2d1341461c4d3a1fe7abe1864ac24f5aa
ThiagoDuart <td1405150+github@gmail.com>
2024-01-15 14:30:42 -0300

    feat: ensure addAccountRepositore is called with the passed parameters

--- src/data/usecases/db_add_account/DbAddAccount.ts
+++ src/data/usecases/db_add_account/DbAddAccount.ts
@@ -1,13 +1,21 @@
-import { IEncrypter } from "data/protocols";
+import { IAddAccountRepositore, IEncrypter } from "data/protocols";
 import { IAccount, IAccountModel, IAddAccount } from "domain";
 
 export class DbAddAccount implements IAddAccount {
   private readonly encrypter: IEncrypter;
-  constructor(encrypter: IEncrypter) {
+  private readonly addAccountRepositore: IAddAccountRepositore;
+  constructor(
+    encrypter: IEncrypter,
+    addAccountRepositore: IAddAccountRepositore
+  ) {
     this.encrypter = encrypter;
+    this.addAccountRepositore = addAccountRepositore;
   }
   async add(data: IAccountModel): Promise<IAccount> {
     const passwordHash = await this.encrypter.encrypt(data.password);
+    const account = await this.addAccountRepositore.add(
+      Object.assign({}, data, { password: passwordHash })
+    );
     return;
   }
 }
