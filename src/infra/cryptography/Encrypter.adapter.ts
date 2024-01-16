dac09b4bffed3265cd9b4f19469d42b76f15ffdd
ThiagoDuart <td1405150+github@gmail.com>
2024-01-16 11:02:48 -0300

    feat: should hash is called with the passed parameters

---
+++ src/infra/cryptography/Encrypter.adapter.ts
@@ -0,0 +1,9 @@
+import { IEncrypter } from "data/protocols";
+import bcrypt from "bcrypt";
+
+export class Encrypter implements IEncrypter {
+  async encrypt(password: string, salt: number): Promise<string> {
+    const passwordHash = await bcrypt.hash(password, 10);
+    return passwordHash;
+  }
+}
