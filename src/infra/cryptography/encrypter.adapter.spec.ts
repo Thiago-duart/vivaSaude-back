7b1653743e1b90330174ffa0393249d826c66b15
ThiagoDuart <td1405150+github@gmail.com>
2024-01-16 11:31:58 -0300

    test: case returns a correct hash

--- src/infra/cryptography/encrypter.adapter.spec.ts
+++ src/infra/cryptography/encrypter.adapter.spec.ts
@@ -7,11 +7,23 @@ describe("Encrypter.adapter", () => {
       sut,
     };
   }
-
   test("should hash is called with the passed parameters", async () => {
     const { sut } = makeSut();
     const hashSpy = jest.spyOn(bcrypt, "hash");
     await sut.encrypt("any_value", 10);
     expect(hashSpy).toHaveBeenCalledWith("any_value", 10);
+  });
+
+  test("case returns a correct hash", async () => {
+    const { sut } = makeSut();
+    jest
+      .spyOn(sut, "encrypt")
+      .mockImplementationOnce(
+        async (password: string, salt: number): Promise<string> => {
+          return "any_value";
+        }
+      );
+    const hash = await sut.encrypt("any_value", 10);
+    expect(hash).toBe("any_value");
   });
 });
