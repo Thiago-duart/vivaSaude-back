137e3e1a35dea1fa9076f30d2fb56e6019791ef9
ThiagoDuart <td1405150+github@gmail.com>
2024-01-19 16:07:03 -0300

    feat: should return account if successful

---
+++ src/infra/mongodb/account-repositore/account.ts
@@ -0,0 +1,19 @@
+import { IAddAccountRepositore } from "@/data/protocols";
+import { IAccountModel, IAccount } from "@/domain";
+import { mongoConnection } from "../helper/mongo-helper";
+
+export class AccountMongoRepositore implements IAddAccountRepositore {
+  async add(data: IAccountModel): Promise<IAccount> {
+    const accountCollection = mongoConnection.getCollection("accounts");
+    const response = await accountCollection.insertOne(data);
+    const { _id, ...accountData } = await accountCollection.findOne({
+      _id: response.insertedId,
+    });
+    return {
+      id: _id.toString(),
+      name: accountData.name,
+      email: accountData.email,
+      password: accountData.password,
+    };
+  }
+}
