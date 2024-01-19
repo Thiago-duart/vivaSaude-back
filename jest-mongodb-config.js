4b5cc50bbe74a021bdc75510f0a94d531959975e
ThiagoDuart <td1405150+github@gmail.com>
2024-01-19 16:05:17 -0300

    refactor: configs and jest-mongodb and tsconfig.json

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,10 +1,12 @@
-module.exports = {
-    mongodbMemoryServerOptions: {
-      binary: {
-        version: '4.0.3',
-        skipMD5: true,
-      },
-      autoStart: false,
-      instance: {},
-    },
-  };
+módulo.exportações  =  { 
+  mongodbMemoryServerOptions : { 
+    binário : { 
+      versão : '6.3.0' , 
+      skipMD5 : true , 
+    } , 
+    instância : { 
+      dbName : 'jest' , 
+    } , 
+    autoStart : false , 
+  } , 
+};
