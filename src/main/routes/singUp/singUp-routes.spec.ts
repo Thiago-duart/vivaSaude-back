f4fd4ad037c098ddf857666929af3b3e2f215549
ThiagoDuart <td1405150+github@gmail.com>
2024-01-29 12:17:42 -0300

    test: should return status 201 and account data

--- src/main/routes/singUp/singUp-routes.spec.ts
+++ src/main/routes/singUp/singUp-routes.spec.ts
@@ -1,6 +1,19 @@
 import request from "supertest";
 import app from "../../config/app";
+import { mongoConnection } from "../../../infra/mongodb/helper/mongo-helper";
 describe("singUp-routes", () => {
+  beforeAll(async () => {
+    await mongoConnection.connect(global.__MONGO_URI__);
+  });
+
+  afterAll(async () => {
+    await mongoConnection.desconect();
+  });
+
+  beforeEach(async () => {
+    const collections = mongoConnection.getCollection("accounts");
+    await collections.deleteMany({});
+  });
   test("should return status 201 and account data", async () => {
     await request(app)
       .post("/viva/singup")
@@ -9,7 +22,6 @@ describe("singUp-routes", () => {
         email: "td1405150@gmail.com",
         password: "valid_password",
       })
-      .expect({ id: "valid_id", name: "thiago" })
       .expect(201);
   });
 });
