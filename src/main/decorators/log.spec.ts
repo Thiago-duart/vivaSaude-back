f12c85aec8b4480e6fba6daf6037e589267e7f2f
ThiagoDuart <td1405150+github@gmail.com>
2024-02-02 10:54:37 -0300

    test: should return the same as the controller returns

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -9,7 +9,11 @@ describe("log", () => {
   function makeSut() {
     class ControllerStub implements IController {
       handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        return;
+        const fakeResponse: IHttpResponse = {
+          statusCode: 200,
+          body: { name: "thiago" },
+        };
+        return new Promise((Resolve) => Resolve(fakeResponse));
       }
     }
     const controllerStub = new ControllerStub();
@@ -27,5 +31,17 @@ describe("log", () => {
     };
     await logControllerdDecorator.handle(httpRequest);
     expect(controllerSpy).toHaveBeenCalled();
+  });
+  test("should return the same as the controller returns", async () => {
+    const { logControllerdDecorator } = makeSut();
+    const httpRequest = {
+      body: {},
+    };
+    const httpResponse: IHttpResponse = {
+      statusCode: 200,
+      body: { name: "thiago" },
+    };
+    const response = await logControllerdDecorator.handle(httpRequest);
+    expect(response).toEqual(httpResponse);
   });
 });
