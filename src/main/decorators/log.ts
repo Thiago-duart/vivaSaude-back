22107f87a701c793c507a4e91e49bda57c7e112a
ThiagoDuart <td1405150+github@gmail.com>
2024-02-02 12:17:26 -0300

    feat: should call LogErrorRepositore if controller handle returns serverError

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,3 +1,4 @@
+import { ILogErrorRepositore } from "../../data/protocols";
 import {
   IController,
   IHttpRequest,
@@ -6,11 +7,19 @@ import {
 
 export class LogControllerdDecorator implements IController {
   private readonly controller: IController;
-  constructor(controller: IController) {
+  private readonly logErrorRepositore: ILogErrorRepositore;
+  constructor(
+    controller: IController,
+    logErrorRepositore: ILogErrorRepositore
+  ) {
     this.controller = controller;
+    this.logErrorRepositore = logErrorRepositore;
   }
   async handle(IHttpRequest: IHttpRequest): Promise<IHttpResponse> {
     const response = await this.controller.handle(IHttpRequest);
+    if (response.statusCode == 500) {
+      await this.logErrorRepositore.log(response.body?.stack);
+    }
     return response;
   }
 }
